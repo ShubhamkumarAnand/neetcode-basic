f949d6cddeadde0b935442ebcb0999e66e354cb2
imskanand <the.anand3099@gmail.com>
2022-09-12 17:43:49 +0530

    Initial Commit - Static Array

---
+++ 1. Static Array/Neetcode.cpp
@@ -0,0 +1,57 @@
+/*
+   * Author :  imskanand
+   ^ Time   : 12 September 2022 (17:32)
+*/
+
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using i64 = long long;
+
+void takingInput(int arr[], int length) {
+  for (int i = 0; i < length; i++) {
+    cin >> arr[i];
+  }
+}
+
+void readingOutput(int arr[], int length) {
+  for (int i = 0; i < length; i++) {
+    cout << arr[i] << ' ';
+  }
+  cout << '\n';
+}
+
+void insertingEnd(int arr[], int length, int capacity, int number) {
+  if (length < capacity) {
+    arr[length] = number;
+  }
+}
+
+void removingEnd(int arr[], int length) {
+  if (length > 0) {
+    arr[length - 1] = 0;
+  }
+}
+
+void insertMiddle(int arr[], int length, int index, int value) {
+  for (int i = 0; i < length; i++) {
+    arr[i + 1] = arr[i];
+  }
+  arr[index] = value;
+}
+
+void removeMiddle(int arr[], int length, int index) {
+  for (int i = index + 1; i < length; i++) {
+    arr[i + 1] = arr[i];
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
